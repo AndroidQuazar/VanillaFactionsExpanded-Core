5a9390ec506cbb597b64a0c5b43fcf60ccf6ea45
Simon Latusek <simon.latusek@gmail.com>
2020-08-26 05:32:56 +0200

    Initial defs

---
+++ Source/VFECore/Defs/RenderAddonDef.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace VFECore.Defs
+{
+    using RimWorld;
+    using Verse;
+
+    public class RenderAddonDef : Def
+    {
+        public List<RenderAddonLayerDef> layers;
+
+        private GraphicData graphicData;
+
+        public RenderAddonColoringMode coloring = RenderAddonColoringMode.Custom;
+
+        public Graphic Graphic =>
+            this.graphicData?.Graphic;
+    }
+
+    public class RenderAddonLayerDef : Def
+    {
+        public float offset = 0f;
+    }
+
+    public enum RenderAddonColoringMode
+    {
+        Static,
+        Custom,
+        Hair
+    }
+}
